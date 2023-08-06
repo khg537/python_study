7fa1b31fa3b3e0e73ab0953cfcdef066f4c70e5d
khg537 <khg537@gmail.com>
2023-08-07 02:00:12 +0900

    Update p5g_main.py

--- p5g/p5g_main.py
+++ p5g/p5g_main.py
@@ -21,10 +21,14 @@ from PyQt5.QtCore import QWaitCondition
 from PyQt5.QtCore import QMutex
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtCore import pyqtSlot
+
+import subprocess
 import pickle
 from P5GLog import PLog
 
-g_lists =[]
+from os import path
+
+g_lists = []
 drop_cnt = 0
 
 form_class = uic.loadUiType("p5g_merge.ui")[0]
@@ -42,7 +46,18 @@ class MyWindow(QMainWindow, form_class):
         self.PB_MERGE.clicked.connect(self.MergeFile)
         self.PB_CLEAR.clicked.connect(self.ClearFile)
         
+        self.PB_MSG.clicked.connect(self.MsgFile)
+        self.PB_EXPLORER.clicked.connect(self.FileExplorer)
+        self.PB_NOTEPAD.clicked.connect(self.RunNotepad)
+        self.PB_QDIR.clicked.connect(self.RunQdir)
+        
         self.file_df = None
+        self.message_df = None
+        
+        self.cur_dir = self.ReadFileDir()
+        
+        self.textBrowser.setPlainText("")
+        self.label_select.setText("0 개 선택")
         self.Log = PLog()
        
         
@@ -51,11 +66,14 @@ class MyWindow(QMainWindow, form_class):
         #                                 "All Files(*);; log Files(*.log)", '/home')
         
         # Directory 를 선택합니다.
-        folder = QFileDialog.getExistingDirectory(self, "Select Directory")
-        print(folder)
+        folder = QFileDialog.getExistingDirectory(self, "Select Directory", self.cur_dir)
+   
         
         self.Log.add(folder, None)
         
+        self.PrintFiles()
+        self.cur_dir = os.getcwd()
+        self.WriteFileDir()
 
         # C:/Users/combr/PycharmProjects/somePath/venv/Lib
         
@@ -70,20 +88,87 @@ class MyWindow(QMainWindow, form_class):
         # (['C:/Users/combr/easy-install.pth', 'C:/Users/combr/setuptools.pth'], '*.pth')
 
     def OnFiles(self):
-        files = QFileDialog.getOpenFileNames(self, 'Choose Files',  filter='*.*')
+        files = QFileDialog.getOpenFileNames(self, 'Choose Files', self.cur_dir,  filter='*.*')
         fnames = files[0]
+        # print(files)
+        print(os.getcwd())
         self.Log.add(None, fnames)
+        self.PrintFiles()
+        self.cur_dir = os.getcwd()
+        self.WriteFileDir()
 
     def MergeFile(self):
         self.file_df = self.Log.MergeFile()
         t_time = datetime.datetime.now()
         fname = t_time.strftime("Merge_%Y_%m_%d_%H_%M_%S.log")
         self.file_df.to_csv(fname, sep='\t', index=False)
+        # print(self.file_df)
+        self.label_select.setText(f"{fname} 생성")
+        
 
     def ClearFile(self):
-        pass
+        self.Log.clear_file()
+        self.textBrowser.setPlainText("")
+        self.label_select.setText("0 개 선택")
 
+    def PrintFiles(self):
+        i = 0
+        files = self.Log.get_files()
+        for i, file in enumerate(files):
+            self.textBrowser.append(file)
+            
+        self.label_select.setText(f"{i+1}개 선택")
+        
+    def MsgFile(self):
+        
+        self.message_df = self.Log.MakeMessage()
+        self.message_df.columns = ['time', 'value']
 
+        self.message_df['time'] = pd.to_datetime(self.message_df['time'])
+        self.message_df = self.message_df.sort_values(by='time')  
+       
+        cont_series = self.message_df.iloc[:,1]
+        for i, cont in cont_series.items():
+            self.print_list_prettier(cont)
+        
+    def print_list_prettier(self, prt_list):
+        t_time = datetime.datetime.now()
+        fname = t_time.strftime("Message_%Y_%m_%d_%H_%M_%S.log")
+        
+        for i in prt_list:
+            with open(fname, "a") as f:
+                f.write(i + "\n")
+              
+        self.label_select.setText(f"\n{fname} 생성")
+        
+    def FileExplorer(self):
+        self.cur_dir = self.ReadFileDir()
+        print("222",self.cur_dir)
+        os.startfile(self.cur_dir)
+        
+    def ReadFileDir(self):
+        if path.exists('Filedir.cfg'):
+           with open("Filedir.cfg") as f:
+                return(f.readline().strip())
+          
+        else:
+            self.cur_dir = "c:\\"
+            self.WriteFileDir()
+        
+        if self.cur_dir == None:
+            self.cur_dir = "c:\\"
+    
+    def WriteFileDir(self):
+        with open("Filedir.cfg", "w") as f:
+            f.write(self.cur_dir)
+            
+    def RunNotepad(self):
+        subprocess.call(["C:\\Program Files\\Notepad++\\notepad++.exe"])
+        
+    def RunQdir(self):
+        subprocess.call(["C:\\Program Files (x86)\\Q-Dir\\Q-Dir.exe"])
+
+        
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     myWindow = MyWindow()
